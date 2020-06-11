04b018a93f0b3b7be9db61ceecc4197a0343f2ea
sundaydavid <sundaydavid989@gmail.com>
2020-06-11 22:32:15 +0100

    added local repository

---
+++ app/src/main/java/com/swachev/dataSource/local/LocalRepository.kt
@@ -0,0 +1,10 @@
+package com.swachev.dataSource.local
+
+import androidx.lifecycle.LiveData
+import com.swachev.model.Content
+
+interface LocalRepository {
+
+    fun getStoreItems(): LiveData<List<Content>>
+    suspend fun setStoreItems(response: List<Content>)
+}
