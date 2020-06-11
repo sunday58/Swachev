24ef00080ab4f90696f479489fb9f2386f0351f3
sundaydavid <sundaydavid989@gmail.com>
2020-06-11 04:19:24 +0100

    finish setting up api

--- app/src/main/java/com/swachev/dataSource/remote/StoresApi.kt
+++ app/src/main/java/com/swachev/dataSource/remote/StoresApi.kt
@@ -1,6 +1,11 @@
 package com.swachev.dataSource.remote
 
-class StoresApi {
+import com.swachev.model.StoreItems
+import retrofit2.Call
+import retrofit2.http.GET
 
+interface StoresApi {
+@GET("api/v1/store/all")
+fun getStores(): Call<StoreItems>
 
 }
--- app/src/main/java/com/swachev/utility/Constants.kt
+++ app/src/main/java/com/swachev/utility/Constants.kt
@@ -2,5 +2,5 @@ package com.swachev.utility
 
 object Constants {
 
-    const val BASE_URL = "http://evcharge-app-server2-dev.us-west-2.elasticbeanstalk.com/api/v1/store/all"
+    const val BASE_URL = "http://evcharge-app-server2-dev.us-west-2.elasticbeanstalk.com/"
 }
