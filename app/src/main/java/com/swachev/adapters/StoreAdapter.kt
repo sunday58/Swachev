5061814a203c38e61ea0296732916fa9a8b906ee
sundaydavid <sundaydavid989@gmail.com>
2020-06-11 22:57:50 +0100

    adding store adapter class

---
+++ app/src/main/java/com/swachev/adapters/StoreAdapter.kt
@@ -0,0 +1,11 @@
+package com.swachev.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class StoreAdapter {
+
+    class StoreViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
